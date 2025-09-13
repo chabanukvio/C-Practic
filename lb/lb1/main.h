1d93dc846d2bfc6d37fbb0592bdcaf4da0690dc2
chabanukvio <chabanuk3232@gmail.com>
2025-09-13 14:59:51 +0300

    Твое сообщение о коммите

---
+++ lb/lb1/main.h
@@ -0,0 +1,13 @@
+//
+// Created by User1 on 13.09.25.
+//
+
+#ifndef UNTITLED_MAIN_H
+#define UNTITLED_MAIN_H
+
+
+class main {
+};
+
+
+#endif //UNTITLED_MAIN_H
---
+++ lb/lb2/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User1 on 13.09.25.
+//
+
+#include "main.h"
